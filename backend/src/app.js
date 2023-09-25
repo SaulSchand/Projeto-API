2273dcf0a48fb41985233b7e26371249e8d72b9a
Caio Augusto Exteckoetter <62160930+caioaex@users.noreply.github.com>
2023-09-25 13:41:04 -0300

    commit 2

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = require('./router')
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(router);
 app.use(express.json())
-
+app.use(bodyParser.urlencoded({ extended: true }))
 
 module.exports = app;
